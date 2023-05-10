1d41adcf7fc3cea864d795d2e3bfba716d94b3df
hugo-bourgey <bourgey18@gmail.com>
2023-05-10 11:12:56 +0200

    zebi

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthServiceService } from 'src/services/auth-service.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
+  constructor(
+    private authService: AuthServiceService
+  ) { }
 }
